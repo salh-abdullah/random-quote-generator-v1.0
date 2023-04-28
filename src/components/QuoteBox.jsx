42355759f01034002e7f481c23922831406139c1
salh-abdullah <salhabdullah702@gmail.com>
2023-04-28 19:35:52 +0300

    Deleted the unnecessary files and codes

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -8,43 +8,6 @@ export default function QuoteBox({ darkMode, setDarkMode }) {
 
 
 
-
-    // const [data, setData] = useState({ data: [] });
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [err, setErr] = useState('');
-
-    // const handleClick = async () => {
-    //     setIsLoading(true);
-
-    //     try {
-    //         const response = await fetch('https://api.quotable.io/random', {
-    //             method: 'GET',
-    //             headers: {
-    //                 Accept: 'application/json',
-    //             },
-    //         });
-
-    //         if (!response.ok) {
-    //             throw new Error(`Error! status: ${response.status}`);
-    //         }
-
-    //         const result = await response.json();
-
-    //         // console.log('result is: ', JSON.stringify(result, null, 4));
-
-    //         setData(result);
-    //     } catch (err) {
-    //         setErr(err.message);
-    //     } finally {
-    //         setIsLoading(false);
-    //     }
-    // };
-
-
-    // useEffect(()=>{
-
-    // })
-
     const [data, setData] = useState([]);
     const fetchData = async () => {
         const response = await fetch(`https://api.quotable.io/random`);
@@ -56,24 +19,8 @@ export default function QuoteBox({ darkMode, setDarkMode }) {
     }, [])
 
 
-
-
-
-
-    // const api_url = "https://api.quotable.io/random";
-
-    // async function getQuote(url) {
-    //     const respone = await fetch(url);
-    //     var data = respone.json();
-    //     console.log(data);
-    // }
-
-    // getQuote(api_url);
-
     return (
         <>
-
-
             <div className="quote-box md:w-2/4 lg:w-1/2 w-3/4 shadow-2xl   duration-75 absolute -translate-x-1/2 -translate-y-1/2 
             rounded-xl bg-[#fff] top-2/4 p-11 left-2/4 text-center">
 
@@ -95,9 +42,9 @@ export default function QuoteBox({ darkMode, setDarkMode }) {
                     <Link href={'https://twitter.com/intent/tweet?text=' + data.content + '     ---' + data.author} target="__blank" >
 
 
-                    <button className=" text-cyan-500 rounded-[25px] border-2 border-cyan-500 w-[150px]
+                        <button className=" text-cyan-500 rounded-[25px] border-2 border-cyan-500 w-[150px]
                     h-[50px] flex items-center justify-center m-[5px] bg-transparent  cursor-pointer">
-                        <FaTwitter className='mr-[10px] w-[20px]' />Tweet</button>
+                            <FaTwitter className='mr-[10px] w-[20px]' />Tweet</button>
                     </Link>
                 </div>
             </div>
